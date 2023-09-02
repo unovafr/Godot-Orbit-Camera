f14bdaafd518ca3350ca4c216f67992cd3ecc100
Florian Richer <florian.richer.97@outlook.com>
2023-09-02 12:49:15 +0200

    Fix add OrbitCamera

--- addons/orbit_camera/plugin.gd
+++ addons/orbit_camera/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("OrbitCamera", "Camera", preload("res://addons/orbit_camera/orbit_camera.gd"), preload("res://addons/orbit_camera/orbit_camera_icon.svg"))
+	add_custom_type("OrbitCamera", "Camera3D", preload("res://addons/orbit_camera/orbit_camera.gd"), preload("res://addons/orbit_camera/orbit_camera_icon.svg"))
 
 func _exit_tree():
 	remove_custom_type("OrbitCamera")
